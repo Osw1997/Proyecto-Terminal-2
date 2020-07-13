60dee0fcdae68f2aa57b9635018ecedd033d6f77
dynamic_pooe <oswaldo.paez@gaindynamics.com>
2020-07-12 19:08:38 -0500

    Fin
    Fin :)

--- Investigacion/Benchmark/ScriptEstadDesc.R
+++ Investigacion/Benchmark/ScriptEstadDesc.R
@@ -1,13 +1,13 @@
 library(readxl)
 library(magrittr)
 library(tidyverse)
-datos <- readxl::read_excel('10mo Semestre UPIITA/PTII/Proyecto-Terminal-2/Investigacion/Benchmark/ResultadosBenchmarkFinalV2.xlsx')
+datos <- readxl::read_excel('10mo Semestre UPIITA/PTII/Proyecto-Terminal-2/Investigacion/Benchmark/ResultadosBenchmarkFinalParliament.xlsx')
 summary(datos)
 
 # Análisis de datos de Apache Marmotta
-apacheMarmotta <- datos %>% filter(TripleStore=='Apache Marmotta')
-avgGeneral = apacheMarmotta %>% select(Tiempo) %>% unlist() %>% as.numeric() %>% mean()
-sdGeneral = apacheMarmotta %>% select(Tiempo) %>% unlist() %>% as.numeric() %>% sd()
+apacheMarmotta <- datos %>% filter(TripleStore=='Parliament')
+avgGeneral = apacheMarmotta %>% select(Tiempo) %>% unlist() %>% as.numeric() %>% mean(na.rm=T)
+sdGeneral = apacheMarmotta %>% select(Tiempo) %>% unlist() %>% as.numeric() %>% sd(na.rm=T)
 
 avgPorLimite = vector(length = 5)
 sdPorLimite = vector(length = 5)
@@ -17,29 +17,36 @@ for (limite in c(250,500,1000,2500,5000)){
                     filter(Limite == limite) %>%
                     select(Tiempo) %>% unlist() %>%
                     as.numeric() %>%
-                    mean()
+                    mean(na.rm=T)
   sdPorLimite[n] = apacheMarmotta %>%
                    filter(Limite == limite) %>%
                    select(Tiempo) %>% unlist() %>%
-                   as.numeric() %>% sd()
+                   as.numeric() %>% sd(na.rm=T)
   n = n + 1
 }
 
-avgPorConsulta = vector(length = 6)
-sdPorConsulta = vector(length = 6)
+avgPorConsulta = vector(length = 10)
+sdPorConsulta = vector(length = 10)
 n = 1
-for (consulta in c('Q1','Q2','Q3','Q4','Q5','Q6')){
+for (consulta in c('Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10')){
   avgPorConsulta[n] = apacheMarmotta %>%
                       filter(Consulta == consulta) %>%
                       select(Tiempo) %>%
                       unlist() %>%
                       as.numeric() %>%
-                      mean()
+                      mean(na.rm=T)
   sdPorConsulta[n] = apacheMarmotta %>%
                      filter(Consulta == consulta) %>%
                      select(Tiempo) %>%
                      unlist() %>%
                      as.numeric() %>%
-                     sd()
+                     sd(na.rm=T)
   n = n + 1
 }
+
+avgGeneral
+sdGeneral
+avgPorLimite
+sdPorLimite
+avgPorConsulta
+sdPorConsulta
